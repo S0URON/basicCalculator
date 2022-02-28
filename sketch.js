359c2f73cfbac6c2c794264d87ea6eb74cbe40d6
elmoknimedamin <elmokni.medamin@gmail.com>
2022-02-28 17:39:52 +0100

    first commit

---
+++ sketch.js
@@ -0,0 +1,33 @@
+let currentValue = "";
+let currentOperation ="";
+let currentResult = 0;
+
+
+document.querySelectorAll("button").forEach((e) => {
+    e.onclick = () => {
+        if(["*","/","+","-"].includes(e.innerHTML)){
+            currentOperation = e.innerHTML;
+            if((currentOperation === "/" || currentOperation === "*") && !currentResult)
+                currentResult = 1;
+            if(currentValue)
+                eval(`currentResult${currentOperation}=${currentValue}`);
+            currentValue = "";
+        }
+        else if( ["0","1","2","3","4","5","6","7","8","9"].includes(e.innerHTML)){
+            currentValue += e.innerHTML;
+        }
+        else if(e.innerHTML === "="){
+            eval(`currentResult${currentOperation}=${currentValue}`);
+            currentValue = "";
+            currentOperation = "";
+        }
+        if(currentResult)
+            document.getElementById("result").innerHTML = "result :" + currentResult;
+
+        console.log("currentValue : ",currentValue);
+        console.log("currentOperation : ",currentOperation);
+        console.log("currentResult:", currentResult);
+    }
+})
+
+//abirmhenni@gmail.com
